920ba2f332330cf295532cb0e70aa21ea0f696f4
Arimsean <arim96kg@gmail.com>
2016-05-03 10:04:22 +0600

    Second change

--- admin.py
+++ admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import	Post
 from .models import	Student
 from .models import	Parents
 
-admin.site.register(Post)
 admin.site.register(Student)
 admin.site.register(Parents)
